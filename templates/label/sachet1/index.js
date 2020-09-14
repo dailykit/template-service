4eb9ec149ef84c0b2999e5fd3cfa9df908599c54
Praveen Bisht <prvnbist@gmail.com>
2020-09-14 15:07:55 +0530

    feat: fetch sachet details

--- templates/label/sachet1/index.js
+++ templates/label/sachet1/index.js
@@ -1,21 +1,27 @@
 import pug from 'pug'
 import QR from 'qrcode'
 
+import client from '../../../lib/graphql'
+
 const label = async data => {
    try {
       const parsed = await JSON.parse(data)
 
+      const { sachet = {} } = await client.request(SACHET, {
+         id: parsed.id
+      })
+
       const qrCode = await QR.toDataURL(
          JSON.stringify({
             sachet: { id: parsed.id },
-            product: { id: parsed.product.id }
+            product: { id: sachet.orderProduct.id }
          })
       )
 
       const compiler = await pug.compileFile(__dirname + '/index.pug')
 
       const response = await compiler({
-         ...parsed,
+         ...sachet,
          qrCode
       })
       return response
@@ -25,3 +31,23 @@ const label = async data => {
 }
 
 export default label
+
+const SACHET = `
+   query sachet($id: Int!) {
+      sachet: orderSachet(id: $id) {
+         id
+         unit
+         quantity
+         ingredient: ingredientName
+         processing: processingName
+         orderProduct: orderMealKitProduct {
+            id
+            orderId
+            recipeProduct: simpleRecipeProduct {
+               id
+               name
+            }
+         }
+      }
+   }
+`
